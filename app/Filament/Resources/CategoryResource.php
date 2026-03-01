dc737a4a284ec38eb0df06a49b6f243df9b5e0f9
Nauman Shiraz <nauman_shiraz@hotmail.com>
2026-03-02 01:42:21 +0500

    category & brand resources completed

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make([
+                    Grid::make()
+                        ->schema([
+                            Forms\Components\TextInput::make('name')
+                                ->required()
+                                ->maxLength(255)
+                                ->live(onBlur: true)
+                                ->afterStateUpdated(fn(string $operation, $state, Set $set) => $set('slug', Str::slug($state))),
+                            Forms\Components\TextInput::make('slug')
+                                ->required()
+                                ->disabled()
+                                ->maxLength(255)
+                                ->dehydrated()
+                                ->unique(Category::class, 'slug', ignoreRecord: true),
+                            Forms\Components\Select::make('parent_id')
+                                ->label('Parent Category')
+                                ->options(function () {
+                                    $categories = Category::all();
+                                    $tree = [];
+
+                                    $buildTree = function ($parentId = null, $prefix = '') use (&$buildTree, $categories, &$tree) {
+                                        $categories->where('parent_id', $parentId)->each(function ($category) use (&$buildTree, $prefix, &$tree) {
+                                            $tree[$category->id] = $prefix . $category->name;
+                                            $buildTree($category->id, $prefix . '— '); // Add indentation for hierarchy
+                                        });
+                                    };
+
+                                    $buildTree();
+
+                                    return $tree;
+                                })
+                                ->nullable()
+                                ->searchable(),
+                            Forms\Components\FileUpload::make('image')
+                                ->image()
+                                ->directory('categories'),
+                            Forms\Components\Toggle::make('is_active')
+                                ->required()
+                                ->default(true),
+                        ])
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('parent.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\DeleteAction::make()
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
