12f219a12eb454b22937ac9c09d90267146a3288
Nauman Shiraz <nauman_shiraz@hotmail.com>
2026-03-08 01:54:48 +0500

    registration and login implemented. Also implemented increment/decrement on cart page

--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -2,10 +2,53 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
 
+#[Title('Cart - TallCart')]
 class CartPage extends Component
 {
+    public $cart_items = [];
+
+    public $grand_total;
+
+    public function mount()
+    {
+        $this->cart_items = CartManagement::getCartItemsFromCookie();
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
+    public function removeItem($product_id)
+    {
+        $this->cart_items = CartManagement::removeCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+
+        $this->dispatch('update-cart-count', total_count: count($this->cart_items))->to(Navbar::class);
+        
+        LivewireAlert::title('Product Removed')
+            ->text('Product has been removed from the cart successfully.')
+            ->position('bottom-end')
+            ->timer(3000)
+            ->success()
+            ->toast()
+            ->show();
+    }
+
+    public function increaseQty($product_id)
+    {
+        $this->cart_items = CartManagement::incrementQuantityToCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
+    public function decreaseQty($product_id)
+    {
+        $this->cart_items = CartManagement::decrementQuantityToCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
     public function render()
     {
         return view('livewire.cart-page');
--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -3,36 +3,63 @@
     <h1 class="text-2xl font-semibold mb-4">Shopping Cart</h1>
     <div class="flex flex-col md:flex-row gap-4">
       <div class="md:w-3/4">
-        <div class="bg-white overflow-x-auto rounded-lg shadow-md p-6 mb-4">
+        <div class="bg-white rounded-lg shadow-md p-6 mb-4">
           <table class="w-full">
             <thead>
               <tr>
-                <th class="text-left font-semibold">Product</th>
-                <th class="text-left font-semibold">Price</th>
-                <th class="text-left font-semibold">Quantity</th>
-                <th class="text-left font-semibold">Total</th>
-                <th class="text-left font-semibold">Remove</th>
+                <th class="text-left font-normal text-black-700">Product</th>
+                <th class="text-left font-normal text-black-700">Price</th>
+                <th class="text-left font-normal text-black-700">Quantity</th>
+                <th class="text-left font-normal text-black-700">Total</th>
+                <th class="text-left font-normal text-black-700">Remove</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td class="py-4">
-                  <div class="flex items-center">
-                    <img class="h-16 w-16 mr-4" src="https://placehold.co/150" alt="Product image">
-                    <span class="font-semibold">Product name</span>
-                  </div>
-                </td>
-                <td class="py-4">$19.99</td>
-                <td class="py-4">
-                  <div class="flex items-center">
-                    <button class="border rounded-md py-2 px-4 mr-2">-</button>
-                    <span class="text-center w-8">1</span>
-                    <button class="border rounded-md py-2 px-4 ml-2">+</button>
-                  </div>
-                </td>
-                <td class="py-4">$19.99</td>
-                <td><button class="bg-slate-300 border-2 border-slate-400 rounded-lg px-3 py-1 hover:bg-red-500 hover:text-white hover:border-red-700">Remove</button></td>
-              </tr>
+              @forelse ($cart_items as $item)
+                <tr wire:key="{{ $item['product_id'] }}">
+                  <td class="py-4">
+                    <div class="flex items-center">
+                      <img class="h-16 w-16 mr-4" src="{{ url('storage', $item['image']) }}" alt="{{ $item['name'] }}">
+
+                      <div class="relative group max-w-[200px]">
+                          <span class="block truncate cursor-pointer">
+                              {{ $item['name'] }}
+                          </span>
+
+                          <div class="absolute left-0 bottom-full mb-2 hidden group-hover:block 
+                                      bg-gray-900 text-white text-xs rounded px-2 py-1 
+                                      whitespace-wrap z-10 max-w-[250px]">
+                              {{ $item['name'] }}
+                          </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="py-4">{{ Number::currency($item['unit_amount'], 'PKR')}}</td>
+                  <td class="py-4">
+                    <div class="flex items-center">
+                      <button wire:click="decreaseQty({{ $item['product_id'] }})" class="border rounded-md px-2 mr-2">-</button>
+                      <span class="text-center">{{ $item['quantity'] }}</span>
+                      <button wire:click="increaseQty({{ $item['product_id'] }})" class="border rounded-md px-2 ml-2">+</button>
+                    </div>
+                  </td>
+                  <td class="py-4">{{ Number::currency($item['total_amount'], 'PKR')}}</td>
+                  <td>
+                    <button wire:click="removeItem({{ $item['product_id'] }})" 
+                      class="bg-slate-300 border-slate-400 rounded-lg px-2 py-1 
+                      hover:bg-red-500 hover:text-white hover:border-red-700">
+                      <span wire:loading.remove="" wire:target="removeItem({{ $item['product_id'] }})">Remove</span>
+                      <span wire:loading wire:target="removeItem({{ $item['product_id'] }})">Remove</span>
+                    </button>
+                  </td>
+                </tr>
+              @empty
+                <tr>
+                  <td colspan="5" class="py-4">
+                    <span class="text-red-600 text-center py-4 block font-semibold">Cart is empty!</span>
+                  </td>
+                </tr>
+              @endforelse
+              
               <!-- More product rows -->
             </tbody>
           </table>
@@ -40,25 +67,27 @@
       </div>
       <div class="md:w-1/4">
         <div class="bg-white rounded-lg shadow-md p-6">
-          <h2 class="text-lg font-semibold mb-4">Summary</h2>
+          <h2 class="text-lg mb-4">Summary</h2>
           <div class="flex justify-between mb-2">
             <span>Subtotal</span>
-            <span>$19.99</span>
+            <span>{{ Number::currency($grand_total, 'PKR')}}</span>
           </div>
           <div class="flex justify-between mb-2">
             <span>Taxes</span>
-            <span>$1.99</span>
+            <span>{{ Number::currency(0, 'PKR') }}</span>
           </div>
           <div class="flex justify-between mb-2">
             <span>Shipping</span>
-            <span>$0.00</span>
+            <span>{{ Number::currency(0, 'PKR') }}</span>
           </div>
           <hr class="my-2">
           <div class="flex justify-between mb-2">
             <span class="font-semibold">Total</span>
-            <span class="font-semibold">$21.98</span>
+            <span class="font-semibold">{{ Number::currency($grand_total, 'PKR')}}</span>
           </div>
-          <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+          @if($cart_items)
+            <a href="/checkout" class="block text-center bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</a>
+          @endif
         </div>
       </div>
     </div>
