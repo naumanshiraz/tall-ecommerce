89a5ae5288a18e83a95fbeddd417442fb1caed33
Nauman Shiraz <nauman_shiraz@hotmail.com>
2026-03-09 00:58:25 +0500

    made checkout values dynamic, added validation and added stripe php sdk

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,54 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Checkout')]
 class CheckoutPage extends Component
 {
+    public $first_name;
+
+    public $last_name;
+
+    public $phone;
+
+    public $address;
+
+    public $city;
+
+    public $state;
+
+    public $country;
+
+    public $zip_code;
+
+    public $payment_method;
+
+    public function placeOrder()
+    {
+       $this->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'country' => 'required',
+            'zip_code' => 'required',
+            'payment_method' => 'required',
+       ]);
+    }
+
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $grand_total = CartManagement::calculateGrandTotal($cart_items);
+
+        return view('livewire.checkout-page', [
+            'cart_items' => $cart_items,
+            'grand_total' => $grand_total
+        ]);
     }
 }
